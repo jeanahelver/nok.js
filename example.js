d2b27f5356dfeb92040cfddd8b818250b6cc2174
jeanahelver <32199119+jeanahelver@users.noreply.github.com>
2019-12-28 17:57:15 +0100

    Create example.js

---
+++ example.js
@@ -0,0 +1,23 @@
+let spr;
+let timer =0
+
+function init(){
+    //sprites are a 2d array -1 is transparent 0-6 are the paterns
+    spr = [
+        [-1,1,-1,1,-1],
+        [1,-1,1,-1,1],
+        [1,-1,-1,-1,1],
+        [1,-1,-1,-1,1],
+        [-1,1,-1,1,-1],
+        [-1,-1,1,-1,-1]
+    ]
+}
+function draw(dt){
+    timer +=0.1*dt
+    nok.clear(0) //clear(patern 0-6)
+    nok.line(10,10,50,25) //line(start x,start y,end x,end y)
+    nok.sprite(spr,20,20) //sprite(data,x,y)
+    nok.rect(Math.floor(timer%6)+1,20+Math.sin(timer)*20,20+Math.cos(timer)*20,10,10) // rect(patern 0-6, x, y, width, height)
+    nok.circle(Math.floor(Math.sin(timer)*10),50,20) //circle(radius,x, y)
+    nok.number(timer.toFixed(2),0,0) //number(value, x, y)
+}
